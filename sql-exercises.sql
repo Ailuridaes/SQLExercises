8d3c72336440fdd85bd1b1a3802437f0b0fcf15c
Ailuridaes <ailuridaes@gmail.com>
2016-08-29 14:38:09 -0700

    Initial commit

---
+++ sql-exercises.sql
@@ -0,0 +1,58 @@
+-- 1.  Write a query to return all category names with their descriptions from the Categories table.
+
+
+-- 2.  Write a query to return the contact name, customer id, company name and city name of all Customers in London
+
+
+-- 3.  Write a query to return all available columns in the Suppliers tables for the marketing managers and sales representatives that have a FAX number 
+
+
+-- 4.  Write a query to return a list of customer id's from the Orders table with required dates between Jan 1, 1997 and Dec 31, 1997 and with freight under 100 units.
+
+
+-- 5.  Write a query to return a list of company names and contact names of all customers from Mexico, Sweden and Germany.
+
+
+-- 6.  Write a query to return a count of the number of discontinued products in the Products table.
+
+
+-- 7.  Write a query to return a list of category names and descriptions of all categories beginning with 'Co' from the Categories table.
+
+
+-- 8.  Write a query to return all the company names, city, country and postal code from the Suppliers table with the word 'rue' in their address. The list should ordered alphabetically by company name.
+
+
+-- 9.  Write a query to return the product id and the quantity ordered for each product labelled as 'Quantity Purchased' in the Order Details table ordered by the Quantity Purchased in descending order.
+
+
+-- 10. Write a query to return the company name, address, city, postal code and country of all customers with orders that shipped using Speedy Express, along with the date that the order was made.
+
+
+-- 11. Write a query to return a list of Suppliers containing company name, contact name, contact title and region description.
+
+
+-- 12. Write a query to return all product names from the Products table that are condiments.
+
+
+-- 13. Write a query to return a list of customer names who have no orders in the Orders table.
+
+
+-- 14. Write a query to add a shipper named 'Amazon' to the Shippers table using SQL.
+
+
+-- 15. Write a query to change the company name from 'Amazon' to 'Amazon Prime Shipping' in the Shippers table using SQL. 
+
+
+-- 16. Write a query to return a complete list of company names from the Shippers table. Include freight totals rounded to the nearest whole number for each shipper from the Orders table for those shippers with orders.
+
+
+-- 17. Write a query to return all employee first and last names from the Employees table by combining the 2 columns aliased as 'DisplayName'. The combined format should be 'LastName, FirstName'.
+
+
+-- 18. Write a query to add yourself to the Customers table with an order for 'Grandma's Boysenberry Spread'.
+
+
+-- 19. Write a query to remove yourself and your order from the database.
+
+
+-- 20. Write a query to return a list of products from the Products table along with the total units in stock for each product. Include only products with TotalUnits greater than 100.
